581554257c9fe157c817d50b39a308aac85dd3fe
Manan Kapoor <23.kapoormanan@gmail.com>
2026-08-20 03:34:07 +0530

    Update MAKER_EMAIL to new address

--- website/app/constants.ts
+++ website/app/constants.ts
@@ -1,7 +1,7 @@
 export const REPO_URL = "https://github.com/manankapoor23/claudget";
 export const MAKER_NAME = "Manan Kapoor";
 export const MAKER_URL = "https://github.com/manankapoor23";
-export const MAKER_EMAIL = "manan@colab91.com";
+export const MAKER_EMAIL = "23.kapoormanan@gmail.com";
 export const RELEASES_URL = `${REPO_URL}/releases/latest`;
 export const ARCH_URL = `${REPO_URL}/blob/main/docs/ARCHITECTURE.md`;
 export const LICENSE_URL = `${REPO_URL}/blob/main/LICENSE`;
